a41269b426f870b2a122ccdd61bfac29aa966d06
pjx19 <pjx1572@gmail.com>
2023-05-01 17:40:51 +0200

    opnieuw beginnen

--- src/main/java/com/example/aiui/HelloApplication.java
+++ src/main/java/com/example/aiui/HelloApplication.java
@@ -2,9 +2,7 @@ package com.example.aiui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene((AnchorPane) root, 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/aiui/HelloController.java
+++ src/main/java/com/example/aiui/HelloController.java
@@ -2,20 +2,13 @@ package com.example.aiui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private SplitPane pane;
-    @FXML
-    private TextField vraag;
-    @FXML
-    private Label antwoord;
+    private Label welcomeText;
 
     @FXML
-    private void onVraagEnter() {
-        String input = vraag.getText();
-        antwoord.setText("You asked: " + input);
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
