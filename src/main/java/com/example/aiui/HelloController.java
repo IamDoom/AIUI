6160998e6663e95cd33a02822fc7c3edd093d820
pjx19 <pjx1572@gmail.com>
2023-05-01 17:29:44 +0200

    v

--- src/main/java/com/example/aiui/HelloController.java
+++ src/main/java/com/example/aiui/HelloController.java
@@ -2,13 +2,20 @@ package com.example.aiui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private SplitPane pane;
+    @FXML
+    private TextField vraag;
+    @FXML
+    private Label antwoord;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onVraagEnter() {
+        String input = vraag.getText();
+        antwoord.setText("You asked: " + input);
     }
 }
