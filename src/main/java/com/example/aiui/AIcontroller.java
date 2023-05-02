0d6b55dc37cd70692bf3934ef0d53ed64473a224
IsoChan070 <22115862@student.hhs.nl>
2023-05-02 18:12:17 +0200

    button clickk

---
+++ src/main/java/com/example/aiui/AIcontroller.java
@@ -0,0 +1,14 @@
+package com.example.aiui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AIcontroller {
+
+    @FXML
+    private Button button;
+
+    public Button getButton() {
+        return button;
+    }
+}
