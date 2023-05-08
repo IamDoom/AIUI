35f14312d593646648a5c8744e688f15dc9362e4
app3 <hamza.ben.ali@hva.nl>
2023-05-08 17:43:52 +0200

    user file en basis voor een gebruiker

--- src/main/java/com/example/aiui/HelloApplication.java
+++ src/main/java/com/example/aiui/HelloApplication.java
@@ -1,18 +1,19 @@
 package com.example.aiui;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
 public class HelloApplication extends Application {
+    User testUser = new User("password","username","email");
+    public String name = testUser.getUsername();
     @Override
     public void start(Stage stage) throws IOException {
+        data data = new data();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 406);
+        stage.setResizable(false);
+        stage.setTitle(name);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/aiui/HelloController.java
+++ src/main/java/com/example/aiui/HelloController.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 
 
+
 public class HelloController {
     @FXML
     private VBox box;
