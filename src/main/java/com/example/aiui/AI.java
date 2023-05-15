6ed5ad7710fd7a81e5cf5c17b341279e45ba0f70
pjx19 <pjx1572@gmail.com>
2023-05-15 10:36:15 +0200

    .

--- src/main/java/com/example/aiui/AI.java
+++ src/main/java/com/example/aiui/AI.java
@@ -1,4 +1,19 @@
 package com.example.aiui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 public class AI {
+    @FXML
+    private Label antwoord;
+    @FXML
+    private Label antwoord2;
+    @FXML
+    private TextField input;
+
+    public void setOnKeyPressed(ActionEvent Enter) {
+        antwoord.setText("jammer genoeg hebben wij nog geen antwoord op de vraag: ");
+        antwoord2.setText(input.getText());
+    }
 }
