dc7dc1318f1fd78e8a056ffe6ee4a509b0ca38c1
IsoChan070 <22115862@student.hhs.nl>
2023-05-15 10:26:38 +0200

    switch gefixed

--- src/main/java/com/example/aiui/Ai.java
+++ src/main/java/com/example/aiui/Ai.java
@@ -1,4 +1,11 @@
 package com.example.aiui;
 
-public class Ai {
+import javafx.event.ActionEvent;
+
+public class AI {
+
+    //public void setOnKeyPressed(ActionEvent Enter) {
+        //antwoord.setText("jammer genoeg hebben wij nog geen antwoord op de vraag: ");
+        //antwoord2.setText(chat.getText());
+    //}
 }
