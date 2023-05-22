0eb753843c686dd483d160d1ce5e116d5feaa483
AdvancedGaming <incedalsrf@gmail.com>
2023-05-22 09:51:46 +0200

    Kleine UI verbeteringen

--- src/main/java/com/example/aiui/AI.java
+++ src/main/java/com/example/aiui/AI.java
@@ -1,27 +1,18 @@
 package com.example.aiui;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import java.io.IOException;
-import java.util.Objects;
+
 public class AI {
     boolean Lightmode = true;
 
     @FXML
-    private Label antwoord;
-    @FXML
-    private Label antwoord2;
+    private ListView<String> chatList;
     @FXML
     private TextField input;
     @FXML
@@ -34,25 +25,41 @@ public class AI {
     private Button submit;
 
 
-        @FXML
-        public void setOnKeyPressed(ActionEvent Enter)  {
-            antwoord.setText("jammer genoeg hebben wij nog geen antwoord op de vraag: ");
-            antwoord2.setText(input.getText());
-        }
-        protected void LightMode () {
-            Base.setStyle("-fx-background-color: #bcc1c4;");
-            sidebar.setStyle("-fx-background-color: #307eb3;");
-            mode.setStyle("-fx-background-radius: 10; -fx-background-color: white; -fx-border-width: 0;");
-            submit.setStyle("-fx-background-color: #307eb3");
-            mode.setText("Darkmode");
-        }
-        protected void Darkmode () {
-            Base.setStyle("-fx-background-color: #000000");
-            sidebar.setStyle("-fx-background-color: #bcc1c4;");
-            mode.setStyle("-fx-background-radius: 10; -fx-background-color: #b6b7ba; -fx-border-width: 0;");
-            submit.setStyle("-fx-background-color: #b6b7ba;");
-            mode.setText("Lightmode");
-        }
+    @FXML
+    public void setOnKeyPressed(ActionEvent Enter) {
+        String userMessage = input.getText();
+
+        // Add user's message to the chat list
+        chatList.getItems().add("User: " + userMessage);
+
+        // Generate an automatic response
+        String automaticResponse = generateResponse(userMessage);
+
+        // Add the automatic response to the chat list
+        chatList.getItems().add("AI: " + automaticResponse);
+
+        // Clear the input field
+        input.clear();
+    }
+
+    protected void LightMode() {
+        Base.setStyle("-fx-background-color: #bcc1c4;");
+        sidebar.setStyle("-fx-background-color: #307eb3;");
+        mode.setStyle("-fx-background-radius: 10; -fx-background-color: white; -fx-border-width: 0;");
+        submit.setStyle("-fx-background-color: #307eb3");
+        mode.setText("Darkmode");
+        chatList.setStyle("-fx-background-color: white; -fx-text-fill: black");
+    }
+
+    protected void Darkmode() {
+        Base.setStyle("-fx-background-color: #000000");
+        sidebar.setStyle("-fx-background-color: #bcc1c4;");
+        mode.setStyle("-fx-background-radius: 10; -fx-background-color: #b6b7ba; -fx-border-width: 0;");
+        submit.setStyle("-fx-background-color: #b6b7ba;");
+        mode.setText("Lightmode");
+        chatList.setStyle("-fx-background-color: black; -fx-text-fill: white");
+    }
+
     @FXML
     protected void Toggle() {
         if (Lightmode) {
@@ -63,5 +70,16 @@ public class AI {
             LightMode();
         }
 
+    }
+
+    private String generateResponse(String userMessage) {
+        // Replace this logic with your own or use an AI/chatbot API
+        if (userMessage.equalsIgnoreCase("hello")) {
+            return "Hi! How can I assist you?";
+        } else if (userMessage.equalsIgnoreCase("How are you?") || userMessage.toLowerCase().startsWith("how are you")) {
+            return "As an AI, I don't have feelings or emotions, but I'm functioning as intended and ready to help you. How can I assist you today?";
+        } else {
+                return "I don't have an answer for that now";
+        }
     }
 }
