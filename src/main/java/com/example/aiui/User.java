aa3a437c17a938318136a49a27b97949a6e2c8aa
AdvancedGaming <incedalsrf@gmail.com>
2023-05-22 10:31:21 +0200

    data en user uit elkaar gehaald

---
+++ src/main/java/com/example/aiui/User.java
@@ -0,0 +1,90 @@
+package com.example.aiui;
+
+class User {
+    private static int IDcounter = 1;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String Username;
+    private int employeeID;
+    private String Email;
+    private boolean administrator;
+
+    public User(String firstName, String lastName, String password, String username, int employeeID, String email, boolean administrator) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        Username = username;
+        this.employeeID = employeeID;
+        Email = email;
+        this.administrator = administrator;
+    }
+
+    public User(String firstName, String lastName, String password, String username, String email, boolean administrator) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        Username = username;
+        IDcounter += 1;
+        this.employeeID = IDcounter;
+        Email = email;
+        this.administrator = administrator;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public boolean isAdministrator() {
+        return administrator;
+    }
+
+    public void setAdministrator(boolean administrator) {
+        this.administrator = administrator;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+
+}
