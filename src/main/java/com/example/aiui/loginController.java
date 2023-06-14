235fafe082835585a852245aa3b1c18ddb1c7400
app3 <hamza.ben.ali@hva.nl>
2023-06-14 17:04:45 +0200

    DB en user parameter gefixed

--- src/main/java/com/example/aiui/loginController.java
+++ src/main/java/com/example/aiui/loginController.java
@@ -13,6 +13,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -91,9 +93,8 @@ public class loginController implements Initializable {
     @FXML
     public void login(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mainScene.fxml"));
+        loader.setControllerFactory(type -> new mainController(DB,user));
         root = loader.load();
-        mainController mainController = new mainController(DB, user);
-        mainController = loader.getController();
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
