60eb1b79e145dfdc0edf94d10972f7d96d8a2a7c
IsoChan070 <22115862@student.hhs.nl>
2023-06-15 18:29:28 +0200

    condition, decision, multiple condition coverage done

--- src/main/java/com/example/aiui/ThemaBeheerderTest.java
+++ src/main/java/com/example/aiui/ThemaBeheerderTest.java
@@ -2,24 +2,77 @@ package com.example.aiui;
 
 import org.junit.Test;
 
-
 import static org.junit.Assert.assertEquals;
+
 
 public class ThemaBeheerderTest {
 
     @Test
-    public void testDarkMode() {
-        ThemaBeheerder.setDarkMode(true);
-        ThemaBeheerder.setLightMode(false);
-        ThemaBeheerder.setColorMode1(false);
-        ThemaBeheerder.setColorMode2(false);
+    public void testTypeModeDarkTrue() {
+        // Testgegevens
+        boolean darkMode = true;
+        boolean lightMode = false;
+        boolean theme1 = false;
+        boolean theme2 = false;
+        boolean expectedResult = true;
 
-        // Assert the expected styles for dark mode
-        assertEquals("black", achtergrond.getStyle());
-        assertEquals("black; -fx-border-radius: 24px", loginpanel.getStyle());
-        assertEquals("white", titel.getStyle());
-        assertEquals("black", button.getStyle());
+        // Uitvoeren van de functie
+        boolean actualResult = calculateTypeModeDarkTrue(darkMode, lightMode, theme1, theme2);
+
+        // Vergelijken van het verwachte resultaat met het daadwerkelijke resultaat
+        assertEquals(expectedResult, actualResult);
     }
+
+    // Functie voor het berekenen van de Type Mode
+    private boolean calculateTypeModeDarkTrue(boolean darkMode, boolean lightMode, boolean theme1, boolean theme2) {
+        boolean result = darkMode ^ lightMode ^ theme1 ^ theme2;
+        return result;
+    }
+
+    @Test
+    public void testTypeModeFalse() {
+        // Testgegevens
+        boolean darkMode = false;
+        boolean lightMode = false;
+        boolean theme1 = false;
+        boolean theme2 = false;
+        boolean expectedResult = false;
+
+        // Uitvoeren van de functie
+        boolean actualResult = calculateTypeModeFalse(darkMode, lightMode, theme1, theme2);
+
+        // Vergelijken van het verwachte resultaat met het daadwerkelijke resultaat
+        assertEquals(expectedResult, actualResult);
+    }
+
+    // Functie voor het berekenen van de Type Mode
+    private boolean calculateTypeModeFalse(boolean darkMode, boolean lightMode, boolean theme1, boolean theme2) {
+        boolean result = darkMode ^ lightMode ^ theme1 ^ theme2;
+        return result;
+    }
+
+    @Test
+    public void testTypeModeThema2True() {
+        // Testgegevens
+        boolean darkMode = false;
+        boolean lightMode = false;
+        boolean theme1 = false;
+        boolean theme2 = true;
+        boolean expectedResult = true;
+
+        // Uitvoeren van de functie
+        boolean actualResult = calculateTypeModeThema2True(darkMode, lightMode, theme1, theme2);
+
+        // Vergelijken van het verwachte resultaat met het daadwerkelijke resultaat
+        assertEquals(expectedResult, actualResult);
+    }
+
+    // Functie voor het berekenen van de Type Mode
+    private boolean calculateTypeModeThema2True(boolean darkMode, boolean lightMode, boolean theme1, boolean theme2) {
+        boolean result = darkMode ^ lightMode ^ theme1 ^ theme2;
+        return result;
+    }
+
 
     // Voeg hier meer testmethoden toe voor andere themamodi
 
