23a431ca0a95c15ad2d8ced7f63685dcbaf520b5
pjx19 <pjx1572@gmail.com>
2023-06-16 17:55:19 +0200

    pls niet login verwijderen

--- src/main/java/com/example/aiui/passwordtest.java
+++ src/main/java/com/example/aiui/passwordtest.java
@@ -1,5 +1,8 @@
 package com.example.aiui;
 import org.junit.Test;
+
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -10,13 +13,12 @@ public class passwordtest {
     @Test
     public void passwordlowstrengthtest(){
         Employee employee = new Employee("", "", "", "", "pass");
-     passwordCheck.WachtWoordCheck(employee.getPassword());
-     passwordCheck.WachtWoordStengthVerwerker(employee.getPassword());
+        passwordCheck.WachtWoordStengthVerwerker(employee.getPassword());
+
     }
-    @Test (expected = IllegalArgumentException.class)
-    public void passwordlengthtest(){
-        Employee employee = new Employee("", "", "", "", "pass");
-        passwordCheck.WachtWoordCheck(employee.getPassword());
+    @Test
+    public void passwordhighstrengthtest(){
+        Employee employee = new Employee("", "", "", "", "Password123*");
         passwordCheck.WachtWoordStengthVerwerker(employee.getPassword());
     }
 
